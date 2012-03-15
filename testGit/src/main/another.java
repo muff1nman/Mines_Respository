6df6a6a8e9dc02112388cb947027b89a5bfccad5
ademaria <ademaria@mines.edu>
2012-03-14 22:09:27 -0600

    Changed Hello to hey

--- testGit/src/main/another.java
+++ testGit/src/main/another.java
@@ -7,7 +7,7 @@ public class another {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");
+		System.out.print("Hey World");
 	}
 
 }
