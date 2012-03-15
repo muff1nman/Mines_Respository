2c980bb68b7eeb194087d5f8b80786ba8652ddfa
ademaria <ademaria@mines.edu>
2012-03-14 23:34:33 -0600

    Changed to greetings

--- testGit/src/main/another.java
+++ testGit/src/main/another.java
@@ -7,7 +7,7 @@ public class another {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hi World");
+		System.out.print("Greetings World");
 	}
 
 }
