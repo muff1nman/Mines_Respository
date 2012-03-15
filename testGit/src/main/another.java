e64b6ba38b8fc639d9161da2bea83060c0a26f7c
ademaria <ademaria@mines.edu>
2012-03-14 22:07:09 -0600

    Added another.java

---
+++ testGit/src/main/another.java
@@ -0,0 +1,13 @@
+package main;
+
+public class another {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+	}
+
+}
